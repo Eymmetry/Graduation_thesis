173280fc7cdee0ce6354c9d533961f32513af835
muto <momoken98980@gmail.com>
2023-01-29 13:56:29 +0900

    result IDT done

--- Python/test.py
+++ Python/test.py
@@ -3,3 +3,10 @@ import numpy as np
 import math
 import pickle
 np.fft.ifft([0,1j,0,1,0,-1j])
+
+real=7.945427933E-5
+imag=2.120862628E-4
+e=np.sqrt((real)**2+(imag)**2)
+loge=20*np.log10(e)
+print("e=",e)
+print("loge=",loge)
